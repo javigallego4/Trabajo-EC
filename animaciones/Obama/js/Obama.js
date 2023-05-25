a1dbd2b65f93d586d9f6927e67aa005959446d83
Francisco Javier Gallego <javygalleg.m@gmail.com>
2023-05-25 10:22:35 +0200

    añado trabajo

---
+++ animaciones/Obama/js/Obama.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Obama').scianimator({
+	    'images': ['animaciones/Obama/images//Obama1.png', 'animaciones/Obama/images//Obama2.png', 'animaciones/Obama/images//Obama3.png', 'animaciones/Obama/images//Obama4.png', 'animaciones/Obama/images//Obama5.png', 'animaciones/Obama/images//Obama6.png', 'animaciones/Obama/images//Obama7.png', 'animaciones/Obama/images//Obama8.png', 'animaciones/Obama/images//Obama9.png', 'animaciones/Obama/images//Obama10.png', 'animaciones/Obama/images//Obama11.png', 'animaciones/Obama/images//Obama12.png', 'animaciones/Obama/images//Obama13.png', 'animaciones/Obama/images//Obama14.png', 'animaciones/Obama/images//Obama15.png', 'animaciones/Obama/images//Obama16.png', 'animaciones/Obama/images//Obama17.png', 'animaciones/Obama/images//Obama18.png', 'animaciones/Obama/images//Obama19.png', 'animaciones/Obama/images//Obama20.png', 'animaciones/Obama/images//Obama21.png', 'animaciones/Obama/images//Obama22.png', 'animaciones/Obama/images//Obama23.png', 'animaciones/Obama/images//Obama24.png', 'animaciones/Obama/images//Obama25.png', 'animaciones/Obama/images//Obama26.png', 'animaciones/Obama/images//Obama27.png', 'animaciones/Obama/images//Obama28.png', 'animaciones/Obama/images//Obama29.png', 'animaciones/Obama/images//Obama30.png', 'animaciones/Obama/images//Obama31.png', 'animaciones/Obama/images//Obama32.png', 'animaciones/Obama/images//Obama33.png', 'animaciones/Obama/images//Obama34.png', 'animaciones/Obama/images//Obama35.png', 'animaciones/Obama/images//Obama36.png', 'animaciones/Obama/images//Obama37.png', 'animaciones/Obama/images//Obama38.png', 'animaciones/Obama/images//Obama39.png', 'animaciones/Obama/images//Obama40.png', 'animaciones/Obama/images//Obama41.png', 'animaciones/Obama/images//Obama42.png', 'animaciones/Obama/images//Obama43.png', 'animaciones/Obama/images//Obama44.png', 'animaciones/Obama/images//Obama45.png', 'animaciones/Obama/images//Obama46.png', 'animaciones/Obama/images//Obama47.png', 'animaciones/Obama/images//Obama48.png', 'animaciones/Obama/images//Obama49.png', 'animaciones/Obama/images//Obama50.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#Obama').scianimator('play');
+    });
+})(jQuery);

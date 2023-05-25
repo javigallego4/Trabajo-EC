3be6e6f40b79cd1270948d098f6f322d1d4aebbe
Francisco Javier Gallego <javygalleg.m@gmail.com>
2023-05-25 11:56:31 +0200

    corregidos fallos HTML files

---
+++ js/biseccion.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#biseccion').scianimator({
+	    'images': ['animaciones/Biseccion/images//biseccion1.png', 'animaciones/Biseccion/images//biseccion2.png', 'animaciones/Biseccion/images//biseccion3.png', 'animaciones/Biseccion/images//biseccion4.png', 'animaciones/Biseccion/images//biseccion5.png', 'animaciones/Biseccion/images//biseccion6.png', 'animaciones/Biseccion/images//biseccion7.png', 'animaciones/Biseccion/images//biseccion8.png', 'animaciones/Biseccion/images//biseccion9.png', 'animaciones/Biseccion/images//biseccion10.png', 'animaciones/Biseccion/images//biseccion11.png', 'animaciones/Biseccion/images//biseccion12.png', 'animaciones/Biseccion/images//biseccion13.png', 'animaciones/Biseccion/images//biseccion14.png', 'animaciones/Biseccion/images//biseccion15.png', 'animaciones/Biseccion/images//biseccion16.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#biseccion').scianimator('play');
+    });
+})(jQuery);
